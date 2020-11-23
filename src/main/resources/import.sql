57a8ce116b286ec8c1f45d153cbe48295d66ffcb
jfinol02 <yonky45@gmail.com>
2020-11-22 22:51:58 -0300

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO persons( create_at, email, first_name, last_name)	VALUES (NOW(), 'jfinol01@gmail.com', 'Jesus', 'Finol');
+INSERT INTO persons( create_at, email, first_name, last_name)	VALUES (NOW(), 'jfinol02@gmail.com', 'Jesus', 'Finol');
 INSERT INTO persons( create_at, email, first_name, last_name)	VALUES (NOW(), 'jfinol02@gmail.com', 'Rosa', 'Finol');
-INSERT INTO persons( create_at, email, first_name, last_name)	VALUES (NOW(), 'jfinol03@gmail.com', 'Andres', 'Finol');
-INSERT INTO persons( create_at, email, first_name, last_name)	VALUES (NOW(), 'jfinol04@gmail.com', 'Manuel', 'Finol');
-INSERT INTO persons( create_at, email, first_name, last_name)	VALUES (NOW(), 'jfinol05@gmail.com', 'Maria', 'Finol');
+INSERT INTO persons( create_at, email, first_name, last_name)	VALUES (NOW(), 'jfinol02@gmail.com', 'Andres', 'Finol');
+INSERT INTO persons( create_at, email, first_name, last_name)	VALUES (NOW(), 'jfinol02@gmail.com', 'Manuel', 'Finol');
+INSERT INTO persons( create_at, email, first_name, last_name)	VALUES (NOW(), 'jfinol02@gmail.com', 'Maria', 'Finol');
